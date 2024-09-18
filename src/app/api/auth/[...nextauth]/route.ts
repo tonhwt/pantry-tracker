e0418a9ea1fb5e1d27315bd88acc9b4a9a070662
iton.dev <itondevelop@gmail.com>
2024-09-18 22:43:58 +0700

    chore: Update .gitignore to include pnpm-lock.yaml

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
-import { authConfig } from "@/lib/auth";
+import { AuthOptions } from "next-auth";
 import NextAuth from "next-auth";
+import { authConfig } from "@/lib/auth";
 
-const handler = NextAuth(authConfig);
+const handler = NextAuth(authConfig as AuthOptions);
 
 export { handler as GET, handler as POST };
